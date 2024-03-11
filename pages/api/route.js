8a28405ac9e71be4bc87eb576c7bef4b28b4d311
NAMBININA Clavien <110566445+Clavien317@users.noreply.github.com>
2024-03-11 16:34:14 +0300

    Update route.js

--- pages/api/route.js
+++ pages/api/route.js
@@ -8,7 +8,7 @@ export default async function(req, res) {
             service: 'gmail',
             auth: {
                 user: 'Claviennambinina511@gmail.com',
-                pass: 'tzhf xcws msyv knfo'
+                pass: 'qfib nnci dfjt mcxw'
             }
         });
 
